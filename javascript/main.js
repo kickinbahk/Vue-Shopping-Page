fcb7647fe84cad503fb43b52dbaa24adfd1c45b8
kickinbahk <kickinbahk@gmail.com>
2019-11-03 15:47:49 -0800

    add image asset and display image in template

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks to keep your feet warm on a cold, winter day.'
+    description: 'A pair of warm, fuzzy socks to keep your feet warm on a cold, winter day.',
+    image: './assets/vmSocks-green-onWhite.jpg'
   }
 })
