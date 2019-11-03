0855ba022a78d358f55692c23005609b32359bab
kickinbahk <kickinbahk@gmail.com>
2019-11-03 15:25:20 -0800

    add description key and expression

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks'
+    product: 'Socks',
+    description: 'A pair of warm, fuzzy socks to keep your feet warm on a cold, winter day.'
   }
 })
