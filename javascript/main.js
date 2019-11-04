86a01f8acd8381b6c4713225d498945a5222e9df
kickinbahk <kickinbahk@gmail.com>
2019-11-03 16:45:26 -0800

    add variants and display them

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks',
-    description: 'A pair of warm, fuzzy socks to keep your feet warm on a cold, winter day.',
-    image: './assets/vmSocks-green-onWhite.jpg',
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks to keep your feet warm on a cold, winter day.",
+    image: "./assets/vmSocks-green-onWhite.jpg",
     inventory: 100,
     onSale: true,
-    details: ["80% Cotton", "20% Polyester", "Gender-Neutral"]
+    details: ["80% Cotton", "20% Polyester", "Gender-Neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ]
   }
 })
