a80213482c0c135222e9cf6161150dee7b6217b8
kickinbahk <kickinbahk@gmail.com>
2019-12-28 14:26:03 -0800

    make fields required

--- javascript/main.js
+++ javascript/main.js
@@ -133,17 +133,17 @@ Vue.component('product-review', {
     <form class="review-form" @submit.prevent="onSubmit">
       <p>
         <label for="name">Name:</label>
-        <input id="name" v-model="name"/>
+        <input id="name" v-model="name" required />
       </p>
 
       <p>
         <label for="review">Review:</label>
-        <textarea id="review" v-model="review"></textarea>
+        <textarea id="review" v-model="review" required></textarea>
       </p>
 
       <p>
         <label for="rating">Rating:</labe>
-        <select id="rating" v-model.number="rating">
+        <select id="rating" v-model.number="rating" required>
           <option>5</option>
           <option>4</option>
           <option>3</option>
