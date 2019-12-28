bcc7876c36627d335e80a7933418a4f5d97706cb
kickinbahk <kickinbahk@gmail.com>
2019-12-28 13:44:24 -0800

    add fields for review data

--- javascript/main.js
+++ javascript/main.js
@@ -114,10 +114,35 @@ Vue.component('product-details', {
 })
 
 Vue.component('product-review', {
-  template: `<input v-model="name">`,
+  template: `
+    <form class="review-form">
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name"/>
+      </p>
+
+      <p>
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+
+      <p>
+        <label for="rating">Rating:</labe>
+        <select id="rating" v-model="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+    </form>
+  `,
   data() {
     return {
-      name: null
+      name: null,
+      review: null,
+      rating: null
     }
   }
 })
